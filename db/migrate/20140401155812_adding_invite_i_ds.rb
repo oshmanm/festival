9b079250c73f033f2f7cb23bf35ab07e1a0d0ddb
oshmanm <michael.oshman@gmail.com>
2014-04-01 15:51:38 -0400

    update

--- db/migrate/20140401155812_adding_invite_i_ds.rb
+++
@@ -1,8 +0,0 @@
-class AddingInviteIDs < ActiveRecord::Migration
-  def change
-  	change_column :invites, :invitor, :integer
-  	change_column :invites, :invitee, :integer
-  	rename_column :invites, :invitor, :invitor_id
-  	rename_column :invites, :invitee, :invitee_id
-  end
-end
